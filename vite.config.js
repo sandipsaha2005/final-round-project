70619b0e5d3afa0ee00897efec78df0821a9a76e
MRWICKJ <shubhenduhalder4@gmail.com>
2024-09-04 17:58:34 +0530

    initialize stage

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  server: {
-    port: 3001,
-    host: "localhost",
-  },
-  preview: {
-    port: 8001,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    outDir: "prod",
-  },
-});
+})
