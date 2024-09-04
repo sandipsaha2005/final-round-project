11611d524d4bec4e1e7e6599f22b35934dc87431
Sandip Saha <110690069+sandipsaha2005@users.noreply.github.com>
2024-09-04 19:09:15 +0530

    text to image generation added

---
+++ src/page/pageToRender.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ImageGenerator from '../components/text-to-image'
+
+
+const PageToRender = () => {
+  return (
+    <div>
+
+
+
+        <ImageGenerator/>
+    </div>
+  )
+}
+
+export default PageToRender
